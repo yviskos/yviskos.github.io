8eec56653ad234c1bfc5d58430cfd48685eb3782
Yviskos <137009969+yviskos@users.noreply.github.com>
2024-01-13 11:10:51 -0500

    script js no load

--- scripts.js
+++
@@ -1,5 +0,0 @@
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-if (prefersDarkMode === true) {
-    document.getElementById('githublogo').src = 'github-mark-white.jpg';
-}
