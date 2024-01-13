921600c4471a21961a7adb44119835e40cfe0c9a
Yviskos <137009969+yviskos@users.noreply.github.com>
2024-01-13 10:45:45 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (prefersDarkMode === true) {
+    document.getElementById('myImage').src = 'github-mark-black.jpg';
+}
