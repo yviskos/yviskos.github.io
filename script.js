6b7328587feb5a7e35608b011bdc5ff6e3a22957
Novayaki <doraiiderg@proton.me>
2025-07-26 14:02:51 -0400

    Added silly buttons (:

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ theme.checked = isLightMode;
 
 function applyTheme(isLight) {
 	if (isLight) {
-		document.documentElement.style.setProperty('--mantle', '#FFFBEF');
+		document.documentElement.style.setProperty('--mantle', '#FFFFFFC8');
 		document.documentElement.style.setProperty('--foreground', '#1e2326');
-		document.documentElement.style.setProperty('--crust', '#F2EFDF');
+		document.documentElement.style.setProperty('--crust', '#F2EFDFC8');
 		document.documentElement.style.setProperty('--mauve', '#8DA101');
 	} else {
-		document.documentElement.style.setProperty('--mantle', '#272e33');
+		document.documentElement.style.setProperty('--mantle', '#272e33C8');
 		document.documentElement.style.setProperty('--foreground', '#d3c6aa');
-		document.documentElement.style.setProperty('--crust', '#1e2326');
+		document.documentElement.style.setProperty('--crust', '#1e2326C8');
 		document.documentElement.style.setProperty('--mauve', '#a7c080');
 	}
 }
